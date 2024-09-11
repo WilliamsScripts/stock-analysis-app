22936517309f3c5574ac35980602172912325c4a
Williams Williams <williamscherechiwilliams@gmail.com>
2024-09-12 00:11:26 +0100

    Stock Analysis project setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,163 @@
-import Image from "next/image";
+'use client'
+import React, { useEffect, useState } from 'react';
+import ReactSelect, { MultiValue } from 'react-select';
+import { calculateReturns, getStockData } from '@/lib/stock-data';
+
+export type StockData = {
+  Date: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  ticker: string;
+};
+
+type OptionProps = { value: string, label: string }
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [stockData, setStockData] = useState<StockData[]>([]);
+  const [returnsData, setReturnsData] = useState<( StockData & { return: number | null } )[]>([]);
+  const [ticker, setTicker] = useState<OptionProps[]>([{ label: 'Apple', value: 'AAPL' }]);
+  const [startDate, setStartDate] = useState<string>('2023-01-01');
+  const [endDate, setEndDate] = useState<string>('2023-04-06');
+  const [isMounted, setIsMounted] = useState<boolean>(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  const fetchData = async () => {
+    const __tickers = ticker.map(company => company.value);
+    const data = await getStockData(__tickers, startDate, endDate);
+    const returns = calculateReturns(data);
+    setStockData(data);
+    setReturnsData(returns);
+  }
+
+  const handleSelect = (payload: MultiValue<OptionProps>) => {
+    setTicker((prev: OptionProps[]) => {
+      const selectedValues = payload.map(option => option.value);
+      const filteredPrev = prev.filter(item => selectedValues.includes(item.value));
+      const newSelections = payload.filter(option => !prev.some(item => item.value === option.value));
+      return [...filteredPrev, ...newSelections];
+    });
+  };
+
+  return (
+    <div className="container mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-4">Stock Analysis</h1>
+      <form onSubmit={e => { e.preventDefault(); fetchData(); }}>
+        <div className="mb-4">
+          <label className="block text-sm font-medium">Select Stock</label>
+          {isMounted && 
+            <ReactSelect
+              options={[
+                { label: 'American Airlines', value: 'AAL' },
+                { label: 'Apple', value: 'AAPL' },
+                { label: 'Microsoft', value: 'MSFT' },
+                { label: 'Amazon', value: 'AMZN' },
+                { label: 'Google', value: 'GOOGL' }
+              ]}
+              id='stock'
+              defaultValue={ticker}
+              onChange={handleSelect}
+              isMulti={true}
+            />}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className='grid grid-cols-4 items-center gap-3'>
+          <div>
+            <label className="block text-sm font-medium">Start Date</label>
+            <input
+              type="date"
+              className="mt-1"
+              defaultValue={startDate}
+              onChange={e => setStartDate(e.target.value)}
+            />
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium">End Date</label>
+            <input
+              type="date"
+              className="mt-1"
+              defaultValue={endDate}
+              onChange={e => setEndDate(e.target.value)}
+            />
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium">Analysis Type</label>
+            <select name="" id="">
+              <option value="">Select Type</option>
+            </select>
+          </div>
+
+          <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded-md">
+            Analyze
+          </button>
+        </div>
+      </form>
+
+      <div className="grid grid-cols-2 gap-10 my-6">
+        {/* Display stock data */}
+        {stockData.length > 0 && (
+          <div>
+            <h2 className="text-xl font-semibold">Stock Data</h2>
+            <table className="min-w-full table-auto mt-4">
+              <thead>
+                <tr className='bg-red-200'>
+                  <th className='text-left p-2'>Date</th>
+                  <th className='text-left p-2'>Open</th>
+                  <th className='text-left p-2'>High</th>
+                  <th className='text-left p-2'>Low</th>
+                  <th className='text-left p-2'>Close</th>
+                  <th className='text-left p-2'>Volume</th>
+                  <th className='text-left p-2'>Ticker</th>
+                </tr>
+              </thead>
+              <tbody>
+                {stockData.map((row, index) => (
+                  <tr key={index} className='border-b'>
+                    <td className='p-2'>{row.Date}</td>
+                    <td className='p-2'>{row.open}</td>
+                    <td className='p-2'>{row.high}</td>
+                    <td className='p-2'>{row.low}</td>
+                    <td className='p-2'>{row.close}</td>
+                    <td className='p-2'>{row.volume}</td>
+                    <td className='p-2'>{row.ticker}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+
+        {/* Display returns data */}
+        {returnsData.length > 0 && (
+          <div>
+            <h2 className="text-xl font-semibold">Returns Data</h2>
+            <table className="min-w-full table-auto mt-4">
+              <thead>
+                <tr className='border-b bg-red-200'>
+                  <th className='text-left p-2'>Date</th>
+                  <th className='text-left p-2'>Return</th>
+                </tr>
+              </thead>
+              <tbody>
+                {returnsData.map((row, index) => (
+                  <tr key={index} className='border-b'>
+                    <td className='p-2'>{row.Date}</td>
+                    <td className='p-2'>{row.return ? (row.return * 100).toFixed(2) : 0}%</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+      </div>
+
     </div>
   );
 }
